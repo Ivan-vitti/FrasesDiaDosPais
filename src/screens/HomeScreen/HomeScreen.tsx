022dbf1df08995a703c104c865edef11d51f984a
Ivan-vitti <142020964+Ivan-vitti@users.noreply.github.com>
2024-07-13 17:39:22 -0300

    Up01

--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -5,41 +5,49 @@ import GlobalStyles from "../../Styles/GlobalStyles";
 import PHRASES from "../../services/PhrasesMockService";
 import Icon from "react-native-vector-icons/FontAwesome";
 import Styles, { colors } from "./HomeScreen.style";
-
-
-const restaurar = () =>
-    console.log('restaurar');
-
-
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const HomeScreen = () => {
-
     const [phrase, setPhrase] = useState('p0');
 
-    useEffect(() =>{
-        
-        let numberRamdom = Math.floor(Math.random() * 19)
-        console.log(numberRamdom);
-        setPhrase(PHRASES[numberRamdom].phrase);
+    useEffect(() => {
+        const randomPhrase = async () => {
+            const dataOpen = await AsyncStorage.getItem('dataOpen');
+            const dataAtual = new Date().toLocaleDateString();
 
-    },[]);
+            if (dataOpen !== dataAtual) {
+                let numberRamdom = Math.floor(Math.random() * 19);
+                await AsyncStorage.setItem('dataOpen', dataAtual);
+                await AsyncStorage.setItem('idFrase', '' + numberRamdom);
+                setPhrase(PHRASES[numberRamdom].phrase);
+
+            } else {
+                const idFrase = await AsyncStorage.getItem('idFrase');
+                setPhrase(PHRASES[Number(idFrase)].phrase);
+            }
+        };
+        randomPhrase();
+    }, []);
+
+    
+    const restaurar = () => {
+        console.log('restaurar');
+    };
 
     return (
         <View style={[GlobalStyles.container, { backgroundColor: GlobalStyles.body.backgroundColor }]}>
             <Text style={Styles.title}>{I18n.t('phrase_of_day')}</Text>
             <View style={Styles.boxPhrase}>
-                <Text style={Styles.subtitle}>{I18n.t(phrase)}</Text> 
+                <Text style={Styles.subtitle}>{I18n.t(phrase)}</Text>
             </View>
             <View style={GlobalStyles.body}>
-                    <TouchableOpacity style={Styles.buttonPrimary} onPress={() => restaurar()}>
-                        <Icon name={'share-alt-square'} size={40} color={colors.iconColorBotão} />
-                        <Text style={Styles.buttonTextPrimary}>{I18n.t('share')}</Text>
-                    </TouchableOpacity>
-                </View>
-
+                <TouchableOpacity style={Styles.buttonPrimary} onPress={() => restaurar()}>
+                    <Icon name={'share-alt-square'} size={40} color={colors.iconColorBotão} />
+                    <Text style={Styles.buttonTextPrimary}>{I18n.t('share')}</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     );
-}
-
+};
 
 export default HomeScreen;
