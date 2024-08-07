61aa22567e7d048fa8be3724e5bbc973fde5f314
Ivan-vitti <142020964+Ivan-vitti@users.noreply.github.com>
2024-08-07 14:40:07 -0300

    Up01

--- src/screens/ShareScreen/ColorBackground.tsx
+++ src/screens/ShareScreen/ColorBackground.tsx
@@ -1,6 +1,9 @@
 import React, { useState, useRef } from 'react';
-import { View, Modal, Button, StyleSheet, ActivityIndicator } from 'react-native';
+import { View, Modal, ActivityIndicator, Text, TouchableOpacity, ScrollView, StyleSheet } from 'react-native';
 import ColorPicker from 'react-native-wheel-color-picker';
+import Styles from './Customization.style';
+import I18n from '../../util/i18n';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 type ColorBackgroundProps = {
     visible: boolean;
@@ -10,7 +13,7 @@ type ColorBackgroundProps = {
 
 const ColorBackground: React.FC<ColorBackgroundProps> = ({ visible, onClose, onConfirm }) => {
     const [selectedColor, setSelectedColor] = useState<string>('#FFFFFF');
-    const pickerRef = useRef<ColorPicker | null>(null); // Usando useRef para a referência do ColorPicker
+    const pickerRef = useRef<ColorPicker | null>(null);
 
     const handleColorChange = (color: string) => {
         setSelectedColor(color);
@@ -25,56 +28,57 @@ const ColorBackground: React.FC<ColorBackgroundProps> = ({ visible, onClose, onC
 
     return (
         <Modal
-            visible={visible}
             transparent={true}
-            animationType="slide"
+            visible={visible}
             onRequestClose={onClose}
         >
-            <View style={styles.modalContainer}>
-                <View style={styles.pickerContainer}>
-                    <ColorPicker
-                        ref={pickerRef}
-                        color={selectedColor}
-                        swatchesOnly={false}
-                        onColorChange={handleColorChange}
-                        thumbSize={40}
-                        sliderSize={40}
-                        noSnap={true}
-                        row={false}
-                        wheelLoadingIndicator={<ActivityIndicator size={40} />}
-                        sliderLoadingIndicator={<ActivityIndicator size={20} />}
-                        useNativeDriver={false}
-                        useNativeLayout={false}
-                    />
-                    <View style={styles.buttonContainer}>
-                        <Button title="Cancelar" onPress={onClose} />
-                        <Button title="OK" onPress={handleConfirm} />
-                    </View>
+            <View style={Styles.FundoBackground}>
+                <View style={Styles.Container}>
+                    <Text style={Styles.Title}>{I18n.t('Choose_Color')}</Text>
+                    <ScrollView contentContainerStyle={{ flexGrow: 0.3 }}>
+                        <ColorPicker
+                            ref={pickerRef}
+                            color={selectedColor}
+                            swatchesOnly={false}
+                            onColorChange={handleColorChange}
+                            thumbSize={40}
+                            sliderSize={40}
+                            noSnap={true}
+                            row={false}
+                            wheelLoadingIndicator={<ActivityIndicator size={40} />}
+                            sliderLoadingIndicator={<ActivityIndicator size={20} />}
+                            useNativeDriver={false}
+                            useNativeLayout={false}
+                        />
+
+                        <View style={styless.buttonContainerLocal}>
+                            <TouchableOpacity onPress={onClose} style={styless.modalButtonLocal}>
+                                <Icon name="times-circle" style={Styles.iconStyle} />
+                                <Text style={Styles.subtitle}>{I18n.t('Close')}</Text>
+                            </TouchableOpacity>
+
+                            <TouchableOpacity onPress={handleConfirm} style={styless.modalButtonLocal}>
+                                <Icon name="check-circle" style={Styles.iconStyle} />
+                                <Text style={Styles.subtitle}>{I18n.t('Ok')}</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </ScrollView>
                 </View>
             </View>
-        </Modal>
+        </Modal >
     );
 };
 
-const styles = StyleSheet.create({
-    modalContainer: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'rgba(0, 0, 0, 0.5)',
-    },
-    pickerContainer: {
-        width: '80%',
-        backgroundColor: 'white',
-        borderRadius: 10,
-        padding: 20,
-        alignItems: 'center',
-    },
-    buttonContainer: {
+const styless = StyleSheet.create({
+
+    buttonContainerLocal: {
         flexDirection: 'row',
-        justifyContent: 'space-between',
-        width: '100%',
-        marginTop: 20,
+        justifyContent: 'space-evenly', // Para centralizar os botões
+        paddingTop: 40, // Aumente este valor para abaixar mais os botões
+    },
+    modalButtonLocal: {
+        flexDirection: 'column',
+        alignItems: 'center',
     },
 });
 
